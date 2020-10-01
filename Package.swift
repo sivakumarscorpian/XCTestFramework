0adac9c0054ce8cd19029acab484bf233761181a
sivakumarscorpian <sivakumar.sr87@gmail.com>
2020-10-01 23:18:26 +0530

    Update Package.swift
    1. Updated github url with newer one

--- Package.swift
+++ Package.swift
@@ -19,7 +19,7 @@ let package = Package(
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .binaryTarget(name: "XCTestFramework",
-                      url: "https://github.com/sivakumarscorpian/XCTestFramework/archive/1.0.2.zip",
+                      url: "https://github.com/sivakumarscorpian/XCTestFramework/releases/download/1.0.3/XCTestFramework.xcframework.zip",
                       checksum: "e773e78dc93501a54f27f92e8834f1b6e3998209223f27ebe7f33b3c95c5b13b")
     ]
 )
